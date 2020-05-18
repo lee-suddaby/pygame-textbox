8ea21167caf10dee252529ba195187c683d30418
Lee Suddaby <leesuddaby@gmail.com>
2020-05-18 20:37:09 +0100

    Working on stuff

--- textbox/textbox.py
+++ textbox/textbox.py
@@ -1,9 +1,8 @@
 import string
 import pygame as pg
 
-
-ACCEPTED = string.ascii_letters+string.digits+string.punctuation+" "
-
+ACCEPTED = string.printable
+BANNED = [pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN]
 
 class TextBox(object):
     def __init__(self,rect,**kwargs):
@@ -13,13 +12,18 @@ class TextBox(object):
         self.rendered = None
         self.render_rect = None
         self.render_area = None
+        self.offset = 0
         self.blink = True
         self.blink_timer = 0.0
+        self.blink_pos = 0
+        self.old_blink = 0
         self.key_cur = 0
         self.key_uni = None
         self.key_timer = 0.0
         self.back_press = False
         self.back_timer = 0.0
+
+        self.ren_1 = 0.0
         self.process_kwargs(kwargs)
 
     def process_kwargs(self,kwargs):
@@ -52,14 +56,21 @@ class TextBox(object):
                 self.key_cur = 0
             if event.key in (pg.K_RETURN,pg.K_KP_ENTER):
                 self.execute()
+            if event.key == pg.K_LEFT and self.blink_pos > 0:
+                self.blink_pos -= 1
+                self.blink = True
+            if event.key == pg.K_RIGHT and self.blink_pos < len("".join(self.buffer)):
+                self.blink_pos += 1
+                self.blink = True
             elif event.key == pg.K_BACKSPACE:
                 if self.buffer:
                     self.buffer.pop()
                     if not self.back_press:
                         self.back_timer = pg.time.get_ticks()
                         self.back_press = True
-            elif event.unicode in ACCEPTED:
+            elif event.unicode in ACCEPTED and not event.key in BANNED:
                 self.buffer.append(event.unicode)
+                self.blink_pos += 1
                 if self.key_cur != event.key:
                     self.key_timer = pg.time.get_ticks()
                     self.key_cur = event.key
@@ -92,18 +103,26 @@ class TextBox(object):
             self.key_uni = None
 
         new = "".join(self.buffer)
-        if new != self.final:
+        if new != self.final or self.old_blink != self.blink_pos:
+            self.old_blink = self.blink_pos
             self.final = new
             self.rendered = self.font.render(self.final, True, self.font_color)
             self.render_rect = self.rendered.get_rect(x=self.rect.x+2,
                                                       centery=self.rect.centery)
+
+            self.ren_1 = self.font.size("".join(self.buffer[0:self.blink_pos]))[0]
             if self.render_rect.width > self.rect.width-6:
-                offset = self.render_rect.width-(self.rect.width-6)
-                self.render_area = pg.Rect(offset,0,self.rect.width-6,
+                self.offset = self.render_rect.width-(self.rect.width-6)
+                self.ren_1 -= self.offset
+                #if self.ren_1 < 10:
+                #    self.offset += self.ren_1 + 10
+                self.render_area = pg.Rect(self.offset,0,self.rect.width-6,
                                            self.render_rect.height)
             else:
+                self.offset = 0
                 self.render_area = self.rendered.get_rect(topleft=(0,0))
-        if pg.time.get_ticks()-self.blink_timer > 200:
+            print(self.ren_1)
+        if pg.time.get_ticks()-self.blink_timer > 500:
             self.blink = not self.blink
             self.blink_timer = pg.time.get_ticks()
 
@@ -117,4 +136,9 @@ class TextBox(object):
         if self.blink and self.active:
             curse = self.render_area.copy()
             curse.topleft = self.render_rect.topleft
-            surface.fill(self.font_color,(curse.right+1,curse.y,2,curse.h))
+            if curse.left+self.ren_1 > self.rect.width+self.rect.left-6:
+                surface.fill(self.font_color, (self.rect.width+self.rect.left-6, curse.y, 2, curse.h))
+            else:
+                surface.fill(self.font_color, (curse.left+self.ren_1+1, curse.y, 2, curse.h))
+            #surface.fill(self.font_color,(curse.right+1,curse.y,2,curse.h))
+
